d220454c5e7d53690ef3a00e117e74cedd13a023
mkolster <mkolster@unomaha.edu>
2023-11-16 16:13:01 -0600

    Added type hinting in television.py

--- television.py
+++ television.py
@@ -1,53 +1,50 @@
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
-    def __init__(self):
-        self._status = False
-        self._muted = False
-        self._volume = int(self.MIN_VOLUME)
-        self._channel = int(self.MIN_CHANNEL)
+    def __init__(self) -> None:
+        self._status: bool = False
+        self._muted: bool = False
+        self._volume: int = int(self.MIN_VOLUME)
+        self._channel: int = int(self.MIN_CHANNEL)
 
-    def power(self):
+    def power(self) -> bool:
         if not self._status:
             self._status = True
         else:
             self._status = False
         return self._status
 
-    def mute(self):
+    def mute(self) -> bool:
         if self._status:
             self._muted = not self._muted
-            if self._muted:
-                return self._muted
-            else:
-                return self._muted
+        return self._muted
 
-    def channel_up(self):
+    def channel_up(self) -> int:
         if self._status:
             if self._channel < self.MAX_CHANNEL:
                 self._channel += 1
-                return self._channel
+                # return self._channel
             elif self._channel == self.MAX_CHANNEL:
                 self._channel = self.MIN_CHANNEL
-                return self._channel
+                # return self._channel
+            return self._channel
         else:
             return self._channel
 
-    def channel_down(self):
+    def channel_down(self) -> int:
         if self._status:
             if self._channel > self.MIN_CHANNEL:
                 self._channel -= 1
-                return self._channel
             elif self._channel == self.MIN_CHANNEL:
                 self._channel = self.MAX_CHANNEL
-                return self._channel
+            return self._channel
         else:
             return self._channel
 
-    def volume_up(self):
+    def volume_up(self) -> int:
         if self._status:
             self._muted = False
             if self._volume < self.MAX_VOLUME:
@@ -59,7 +56,7 @@ class Television:
         else:
             return self._volume
 
-    def volume_down(self):
+    def volume_down(self) -> int:
         if self._status:
             self._muted = False
             if self._volume > self.MIN_VOLUME:
@@ -71,7 +68,7 @@ class Television:
         else:
             return self._volume
 
-    def __str__(self):
+    def __str__(self) -> str:
         if self._muted:
             return f'Power = [{self._status}], Channel = [{self._channel}], Volume = [{self.MIN_VOLUME}]'
         else:
